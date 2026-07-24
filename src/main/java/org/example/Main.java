c749d51a31c0db76af3111b8b8529f2613df88e0
jonathan kee <jonathankee99@gmail.com>
2026-07-24 12:06:44 +0800

    update file path

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -588,7 +588,7 @@ public class Main {
 
     private static void singleDocument() throws SQLException, IOException {
         // 1. Pass a File object instead of a raw String
-        File input = new File("/Users/jonathankee/examTopicScraper/document/Document1.html");
+        File input = new File("/Users/jonathankee/examTopicScraper/static_page/src/main/resources/Document1.html");
 
         // 2. Specify the File and character encoding (usually "UTF-8")
         Document doc = Jsoup.parse(input, "UTF-8");
@@ -597,7 +597,7 @@ public class Main {
     }
 
     private static void multipleDocuments() throws IOException, SQLException {
-        Path folderPath = Paths.get("/Users/jonathankee/examTopicScraper/document");
+        Path folderPath = Paths.get("/Users/jonathankee/examTopicScraper/static_page/src/main/resources");
         List<File> files = new ArrayList<>();
 
         try (Stream<Path> paths = Files.list(folderPath)) {
