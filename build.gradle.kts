2017c77ccfff8595946d4fe6de9496b138781b24
jonathan kee <jonathankee99@gmail.com>
2026-07-31 13:05:38 +0800

    apply multi-module setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,25 +7,28 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.jsoup:jsoup:1.22.2")
-    implementation("org.postgresql:postgresql:42.7.13")
-}
-
-tasks.shadowJar {
-    archiveClassifier.set("all") // Optional: names the file static_page-1.0-SNAPSHOT-all.jar
-    manifest {
-        attributes(
-            "Main-Class" to "org.example.Main"
-        )
+allprojects {
+    apply(plugin = "java")
+    repositories {
+        mavenCentral()
     }
-}
-tasks.test {
-    useJUnitPlatform()
+    dependencies {
+        testImplementation(platform("org.junit:junit-bom:5.10.0"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+        implementation("org.jsoup:jsoup:1.22.2")
+        implementation("org.postgresql:postgresql:42.7.13")
+    }
+
+    tasks.shadowJar {
+        archiveClassifier.set("all") // Optional: names the file static_page-1.0-SNAPSHOT-all.jar
+        manifest {
+            attributes(
+                "Main-Class" to "org.example.Main"
+            )
+        }
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "static_page"
+include("Download")
